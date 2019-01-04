bd07403ad314f01b3bc05427bd3c281492a87bd4
busaboy1340 <tdlambe@tycho.ncsc.mil>
2019-01-04 12:07:51 -0500

    Updated test script.

--- .ci/integration-tests/run-integration-tests.sh
+++ .ci/integration-tests/run-integration-tests.sh
@@ -10,10 +10,45 @@ docker-compose -f .ci/docker/docker-compose.yml up -d
 
 #cp ../docker/.env .
 #docker-compose -f ../docker/docker-compose.yml up -d
-#cd .ci/docker
-#docker-compose logs -f
-#docker-compose down
 
+#aca_container_id="$(docker ps -aqf "name=hirs-aca")"
+#echo "ACA Container id: $aca_container_id"
+
+tpm2_container_id="$(docker ps -aqf "name=hirs-aca-provisioner-tpm2")"
+echo "TPM2 Container id: $tpm2_container_id"
+
+tpm2_container_status="$(docker inspect $tpm2_container_id --format='{{.State.Status}}')"
+echo "TPM2 Container Status: $tpm2_container_status"
+
+#tpm2_container_running="$(docker inspect $tpm2_container_id --format='{{.State.Running}}')"
+#echo "TPM2 Container Running: $tpm2_container_running"
+
+while [ $tpm2_container_status == "running" ] 
+do
+#  echo "TPM2 Container Status: $tpm2_container_status"
+#  echo "hirs-aca-provisioner-tpm2 is still running..."
+
+  sleep 5 
+
+  tpm2_container_status="$(docker inspect $tpm2_container_id --format='{{.State.Status}}')"
+done
+
+echo "TPM2 Container Status: $tpm2_container_status"
+echo "hirs-aca-provisioner-tpm2 Exited!!"
+
+#echo ""
+#echo "hirs-aca Test Log:"
+#docker logs $aca_container_id
+echo ""
+echo "hirs-aca-provisioner-tpm2 Test Log:"
+docker logs $tpm2_container_id
+
+echo ""
+echo "End of Systems Test, cleaning up..."
+cd .ci/docker
+#cd ../docker
+docker-compose down
+#docker-compose logs -f
 
 # Check to see if Environment Stand-Up is Complete
 # TODO: Refine to handle multiple container IDs
